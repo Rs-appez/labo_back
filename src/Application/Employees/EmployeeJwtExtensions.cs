acdfee4c77425e66d422faf4a41ff2ed2c82526f
Rs-appez <37080870+Rs-appez@users.noreply.github.com>
2025-10-15 16:54:12 +0200

    add Employee Jwt extensions

---
+++ src/Application/Employees/EmployeeJwtExtensions.cs
@@ -0,0 +1,19 @@
+using ParcBack.Domain.Tokens;
+
+namespace ParcBack.Application.Employees;
+
+public static class EmployeeJwtExtensions
+{
+    public static string GenerateToken(this ITokenService tokens, EmployeeDto employee)
+    {
+        return tokens.GenerateToken(
+            userId: employee.Id,
+            email: employee.Email,
+            role: "Employee",
+            extraClaims: new Dictionary<string, string>
+            {
+                { "isActive", employee.IsActive.ToString() },
+            }
+        );
+    }
+}
