bcfc74db938687a19c5dfa39336ea509b313d897
Rs-appez <37080870+Rs-appez@users.noreply.github.com>
2025-10-16 16:07:30 +0200

    fix not saving lastlogin

--- src/Application/Employees/Login/LoginHandle.cs
+++ src/Application/Employees/Login/LoginHandle.cs
@@ -1,6 +1,7 @@
 using ParcBack.Domain.Entities;
 using ParcBack.Domain.Repositories;
 using MediatR;
+using ParcBack.Domain.Abstractions;
 
 
 namespace ParcBack.Application.Employees.Login;
@@ -8,10 +9,12 @@ namespace ParcBack.Application.Employees.Login;
 public class LoginHandler : IRequestHandler<LoginQuery, EmployeeDto?>
 {
     private readonly IEmployeeRepository _repo;
+    private readonly IUnitOfWork _uow;
 
-    public LoginHandler(IEmployeeRepository repo)
+    public LoginHandler(IEmployeeRepository repo, IUnitOfWork uow)
     {
         _repo = repo;
+        _uow = uow;
     }
 
     public async Task<EmployeeDto?> Handle(LoginQuery command, CancellationToken ct)
@@ -21,7 +24,9 @@ public class LoginHandler : IRequestHandler<LoginQuery, EmployeeDto?>
 
         employee.LastLoginAt = DateTime.UtcNow;
         _repo.Update(employee);
-        
+        await _uow.SaveChangesAsync(ct);
+
+
 
         return employee.ToDto();
     }
