8cc78aef165b917b312f1aa86955638001545f34
Rs-appez <37080870+Rs-appez@users.noreply.github.com>
2025-10-20 12:26:12 +0200

    add all needed include

--- src/Infrastructure/Repositories/EmployeeRepository.cs
+++ src/Infrastructure/Repositories/EmployeeRepository.cs
@@ -12,13 +12,19 @@ public class EmployeeRepository : IEmployeeRepository
     public EmployeeRepository(AppDbContext db) => _db = db;
 
     public async Task<Employee?> GetByIdAsync(Guid id, CancellationToken ct = default)
-        => await _db.Employees.Include(e => e.Role).Include(e => e.Tasks).FirstOrDefaultAsync(e => e.Id == id, ct);
+        => await _db.Employees.Include(e => e.Role)
+                              .Include(e => e.Tasks)
+                                    .ThenInclude(t => t.Type)
+                              .FirstOrDefaultAsync(e => e.Id == id, ct);
 
     public async Task<Employee?> GetByEmailAsync(string email, CancellationToken ct = default)
         => await _db.Employees.Include(e => e.Role).Include(e => e.Tasks).FirstOrDefaultAsync(e => e.Email == email, ct);
 
     public async Task<IReadOnlyList<Employee>> ListAsync(Guid? chefId = null, CancellationToken ct = default)
-        => await _db.Employees.Where(e => chefId == null || e.ChiefId == chefId).Include(e => e.Role).AsNoTracking().ToListAsync(ct);
+        => await _db.Employees.Where(e => chefId == null || e.ChiefId == chefId)
+                .Include(e => e.Role)
+                .Include(e => e.Tasks)
+                .AsNoTracking().ToListAsync(ct);
 
     public async Task Register(Employee item, CancellationToken ct = default)
         => await _db.Employees.AddAsync(item, ct);
