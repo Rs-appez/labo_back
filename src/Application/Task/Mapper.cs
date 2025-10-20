3ef389a23664e0c0734dec5d5d4efcd07944bbc1
Rs-appez <37080870+Rs-appez@users.noreply.github.com>
2025-10-20 11:39:54 +0200

    add Emp id in task dto

--- src/Application/Task/Mapper.cs
+++ src/Application/Task/Mapper.cs
@@ -1,14 +1,16 @@
 using ParcBack.Domain.Entities;
 using ParcBack.Application.TaskTypes;
+using ParcBack.Application.Employees;
 
 namespace ParcBack.Application.EmployeeTasks;
 
 public static class Mappers
 {
     public static EmployeeTaskDto ToDto(this EmployeeTask task)
-        => new EmployeeTaskDto(
+        => new(
             task.Id,
             task.Type.ToDto(),
+            task.EmployeeAssigned.Id,
             task.IsCompleted,
             task.IsValidated
         );
--- src/Application/Task/TaskDto.cs
+++ src/Application/Task/TaskDto.cs
@@ -1,10 +1,12 @@
 using ParcBack.Application.TaskTypes;
+using ParcBack.Application.Employees;
 
 namespace ParcBack.Application.EmployeeTasks;
 
 public record EmployeeTaskDto(
     int Id,
     TaskTypeDto Type,
+    Guid EmployeeAssigned,
     bool IsCompleted,
     bool IsValidated
 );
