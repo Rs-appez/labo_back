8fd27de60999fa1b005c09f0b753f39cd745838f
Rs-appez <37080870+Rs-appez@users.noreply.github.com>
2025-10-20 15:21:05 +0200

    fix name

--- src/Domain/Repositories/IEmployeeRepository.cs
+++ src/Domain/Repositories/IEmployeeRepository.cs
@@ -7,8 +7,7 @@ public interface IEmployeeRepository
     Task<Employee?> GetByIdAsync(Guid id, CancellationToken ct = default);
     Task<Employee?> GetByEmailAsync(string email, CancellationToken ct = default);
     Task<IReadOnlyList<Employee>> ListAsync(Guid? chefId = null, CancellationToken ct = default);
-    Task Register(Employee zone, CancellationToken ct = default);
-    void Update(Employee zone);
-    void Remove(Employee zone);
+    Task Register(Employee employee, CancellationToken ct = default);
+    void Update(Employee employee);
+    void Remove(Employee employee);
 }
-
